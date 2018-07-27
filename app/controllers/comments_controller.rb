d051ef98fe1ad855e3185906964971c8e62dd922
Kan <kanyamamoto77@gmail.com>
2018-07-27 11:09:03 -0700

    Ability to add and delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,15 @@
 class CommentsController < ApplicationController
 	before_action :set_pic
+
 	def create
 		@comment = @pic.comments.build(comment_params)
 		@comment.user_id = current_user.id
 
 		if @comment.save
-			flash[:success] = "Comment succeeded"
-			redirect_to root_path
+			respond_to do |format|
+				format.html { redirect_to root_path }
+				format.js
+			end
 		else
 			flash[:alert] = "Check comment form, wrong input"
 			render root_path
@@ -16,9 +19,13 @@ class CommentsController < ApplicationController
 	def destroy
 		@comment = @pic.comments.find(params[:id])
 
-		@comment.destroy
-		flash[:success] = "Comment deleted"
-		redirect_to root_path
+		if @comment.user_id == current_user.id
+			@comment.delete
+			respond_to do |format|
+				format.html { redirect_to root_path }
+				format.js
+			end
+		end
 	end
 
 
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -4,6 +4,8 @@ class Pic < ApplicationRecord
 	has_attached_file :image, styles: { :medium => "640x" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
+	validates :content, length: { minimum: 3, maximum: 300 }
+
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 end
